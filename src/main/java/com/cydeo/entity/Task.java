1a7a514c8277d266f51eb195069ffbbda688be5c
HadiHarmandar <hadiharmandar1@gmail.com>
2023-04-26 15:13:50 -0500

    Part-4

--- src/main/java/com/cydeo/entity/Task.java
+++ src/main/java/com/cydeo/entity/Task.java
@@ -22,13 +22,13 @@ public class Task extends BaseEntity {
     private String taskDetail;
 
     @Enumerated(EnumType.STRING)
-    private Status status;
+    private Status taskStatus;
 
     @Column(columnDefinition = "DATE")
     private LocalDate assignedDate;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "employee_id")
+    @JoinColumn(name = "assigned_employee_id")
     private User assignedEmployee;
 
     @ManyToOne(fetch = FetchType.LAZY)
--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -1,12 +1,17 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.entity.Task;
+import com.cydeo.enums.Status;
 import com.cydeo.mapper.TaskMapper;
 import com.cydeo.repository.TaskRepository;
 import com.cydeo.service.TaskService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class TaskServiceImpl implements TaskService {
@@ -21,26 +26,46 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public List<TaskDTO> listAllTasks() {
-        return null;
+        return taskRepository.findAll().stream().map(taskMapper::convertToDto).collect(Collectors.toList());
     }
 
     @Override
     public void save(TaskDTO task) {
-
+        task.setTaskStatus(Status.OPEN);
+        task.setAssignedDate(LocalDate.now());
+        taskRepository.save(taskMapper.convertToEntity(task));
     }
 
     @Override
     public void update(TaskDTO task) {
+        Optional<Task> task1 = taskRepository.findById(task.getId());
+        Task convertedTask = taskMapper.convertToEntity(task);
 
+        if (task1.isPresent()) {
+            convertedTask.setTaskStatus(task1.get().getTaskStatus());
+            convertedTask.setAssignedDate(task1.get().getAssignedDate());
+            taskRepository.save(convertedTask);
+        }
     }
 
     @Override
     public void delete(Long id) {
-
+        Optional<Task> task = taskRepository.findById(id);
+        if (task.isPresent()) {
+            task.get().setIsDeleted(true);
+            taskRepository.save(task.get());
+        }
     }
 
     @Override
     public TaskDTO findById(Long id) {
+
+        Optional<Task> task = taskRepository.findById(id);
+
+        if (task.isPresent()) {
+            return taskMapper.convertToDto(task.get());
+        }
+
         return null;
     }
 }
