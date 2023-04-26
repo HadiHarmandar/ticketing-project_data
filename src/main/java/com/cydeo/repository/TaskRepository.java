8e9b221e3fb45fc80f488e2e515452c76e20c7eb
HadiHarmandar <hadiharmandar1@gmail.com>
2023-04-26 16:52:16 -0500

    Part-4

--- src/main/java/com/cydeo/repository/TaskRepository.java
+++ src/main/java/com/cydeo/repository/TaskRepository.java
@@ -2,6 +2,18 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    //    int countByProject_ProjectCodeAndProject_ProjectStatus(String projectCode, Status projectStatus);
+
+    @Query("SELECT COUNT(t) FROM Task t WHERE t.project.projectCode = ?1 AND t.taskStatus <> 'COMPLETE'")
+    int totalNonCompletedTasks(String projectCode);
+
+    @Query(value = "SELECT COUNT(*) " +
+            "FROM tasks t JOIN projects p ON t.project_id = p.id " +
+            "WHERE p.project_code = ?1 AND t.task_status = 'COMPLETE'", nativeQuery = true)
+    int totalCompletedTasks(String projectCode);
+
 }
